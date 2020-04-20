6d98f22135ef40d4834a1ac0a0fb5ef097df95be
Dani <meharidaniel1111@gmail.com>
2020-04-20 17:19:03 +0200

    user greeting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function User(props) {
+    return <h1>Welcome back!</h1>
+}
+
+function Guest(props) {
+    return <h1>Sign up, please!</h1>
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+
+    if(isLoggedIn) {
+        return <User />
+    }
+    return <Guest />
+}
+
+ReactDOM.render(<Greeting isLoggedIn= {true} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
